fef94473455a33d0ddafc5c3f91927d3f7ab1d34
shahbazhashmi14a <shahbazhashmi14a@gmail.com>
2024-05-27 02:14:05 +0000

    update

--- Hello.java
+++ Hello.java
@@ -2,6 +2,10 @@ public class Hello
 {
     public static void main(String args[])
     {
-        System,out,println("Hello World");
+        int a = 11;
+        int b = 12;
+        // Suggested code may be subject to a license. Learn more: ~LicenseLog:2335215512.
+        int c = a + b;
+        System.out.println("Hello World " + c);
     }
 }
